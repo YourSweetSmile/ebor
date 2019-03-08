58a58c532918f76daf7de9c3c52b812b2d98692f
yinjw12 <yy1213822>
2019-03-08 17:22:10 +0800

    add the 'security'

---
+++ src/main/java/com/example/ebor/security/UserContext.java
@@ -0,0 +1,36 @@
+package com.example.ebor.security;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.List;
+
+/**
+ * 存储登录用户权限信息
+ */
+public class UserContext {
+    private final int id;
+    private final String userName;
+    private final List<GrantedAuthority> authorities;
+
+    private UserContext(int id, String userName, List<GrantedAuthority> authorities) {
+        this.id = id;
+        this.userName = userName;
+        this.authorities = authorities;
+    }
+
+    public static UserContext create(int id, String userName, List<GrantedAuthority> authorities) {
+        return new UserContext(id, userName, authorities);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public List<GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+}
