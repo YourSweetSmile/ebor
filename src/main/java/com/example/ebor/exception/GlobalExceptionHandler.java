a88b6e15849d1dd995201095c9bd7944351ec632
yinjw12 <yy1213822>
2019-05-15 20:40:14 +0800

    格式化返回参数和异常统一的处理

--- src/main/java/com/example/ebor/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/ebor/exception/GlobalExceptionHandler.java
@@ -1,26 +1,22 @@
 package com.example.ebor.exception;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 import com.example.ebor.config.ResponseInfo;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.multipart.MultipartException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler implements ResponseBodyAdvice<Object> {
@@ -32,16 +28,39 @@ public class GlobalExceptionHandler implements ResponseBodyAdvice<Object> {
         return true;
     }
 
+    /**
+     * 格式化返回参数
+     * @param body
+     * @param returnType
+     * @param selectedContentType
+     * @param selectedConverterType
+     * @param request
+     * @param response
+     * @return
+     */
     @Override
     public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
 
         if(!(body instanceof ResponseInfo)){
+
+            Object code = ((Map) body).get("status");
+            if(null != code && ((Integer)code) == HttpStatus.NOT_FOUND.value()){
+
+                return new ResponseInfo(false, "URL Not Found");
+            }
             return new ResponseInfo(true, body, "请求成功");
         }else{
             return body;
         }
     }
 
+    /**
+     * 异常的统一处理
+     * @param isTrue
+     * @param msg
+     * @param realMsg
+     * @return
+     */
     private ResponseInfo<String> getResult(boolean isTrue, String msg, String realMsg) {
         ResponseInfo res = new ResponseInfo(isTrue, msg);
         logger.error("发生异常:{},响应的异常信息:{}" ,msg, realMsg);
